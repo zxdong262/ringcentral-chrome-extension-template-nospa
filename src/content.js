96b29ca7d85f68056ec08ffa55a565360a09dda2
ZHAO Xudong <zxdong@gmail.com>
2018-12-19 15:08:31 +0800

    Fix styl ref

--- src/content.js
+++ src/content.js
@@ -5,6 +5,8 @@
 
 import createApp from 'ringcentral-embeddable-extension-common/src/no-spa/init'
 import * as config from './config'
+import 'ringcentral-embeddable-extension-common/src/no-spa/style.styl'
+import './custom.styl'
 
 window.addEventListener('load', createApp(config))
 
