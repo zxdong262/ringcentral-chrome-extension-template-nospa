6efac86719fe77f59ce35e4b45b1b95aa63bb2ac
ZHAO Xudong <zxdong@gmail.com>
2019-02-13 15:35:37 +0900

    Support firefox/Chrome same time (#1)

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,9 @@ const sysConfigDefault = require('./config.default')
 const ExtraneousFileCleanupPlugin = require('webpack-extraneous-file-cleanup-plugin')
 const path = require('path')
 const LodashModuleReplacementPlugin = require('lodash-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
 const pack = require('./package.json')
+
 const stylusSettingPlugin =  new webpack.LoaderOptionsPlugin({
   test: /\.styl$/,
   stylus: {
@@ -17,6 +19,18 @@ const opts = {
   minBytes: 3900
 }
 
+const from = path.resolve(
+  __dirname,
+  'node_modules/ringcentral-embeddable-extension-common/src/icons'
+)
+const to1 = path.resolve(
+  __dirname,
+  'dist/icons'
+)
+const to2 = path.resolve(
+  __dirname,
+  'dist-firefox/icons'
+)
 
 let {
   clientID,
@@ -52,7 +66,12 @@ var config = {
     background: './src/background.js',
     standalone: './node_modules/ringcentral-embeddable-extension-common/src/app/standalone.pug',
     app: './node_modules/ringcentral-embeddable-extension-common/src/app/app.js',
-    manifest: './src/manifest.json'
+    manifest: './src/manifest.json',
+    '../dist-firefox/content': './src/content.js',
+    '../dist-firefox/background': './src/background.js',
+    '../dist-firefox/standalone': './node_modules/ringcentral-embeddable-extension-common/src/app/standalone-firefox.pug',
+    '../dist-firefox/app': './node_modules/ringcentral-embeddable-extension-common/src/app/app.js',
+    '../dist-firefox/manifest': './src/manifest-firefox.json'
   },
   output: {
     path: __dirname + '/dist',
@@ -67,7 +86,7 @@ var config = {
   },
   resolveLoader: {
     modules: [
-      path.join(process.cwd(), 'loaders'),
+      path.join(process.cwd(), 'node_modules/ringcentral-embeddable-extension-common/loaders'),
       path.join(process.cwd(), 'node_modules')
     ]
   },
@@ -77,7 +96,7 @@ var config = {
   module: {
     rules: [
       {
-        test: /manifest\.json$/,
+        test: /manifest\.json$|manifest-firefox\.json$/,
         use: [
           'manifest-loader'
         ]
@@ -124,12 +143,22 @@ var config = {
       },
       {
         test: /\.(png|jpg|svg)$/,
-        use: ['url-loader?limit=10192&name=images/[hash].[ext]']
+        use: ['url-loader?limit=101920&name=images/[hash].[ext]']
       },
       {
-        test: /\.pug$/,
+        test: /standalone\.pug$/,
         use: [
           'file-loader?name=./standalone.html',
+          'concat-loader',
+          'extract-loader',
+          'html-loader',
+          pug
+        ]
+      },
+      {
+        test: /standalone-firefox\.pug$/,
+        use: [
+          'file-loader?name=../dist-firefox/standalone.html',
           'concat-loader',
           'extract-loader',
           'html-loader',
@@ -145,6 +174,15 @@ var config = {
       collections: true,
       paths: true
     }),
+    new CopyWebpackPlugin([{
+      from,
+      to: to1,
+      force: true
+    }, {
+      from,
+      to: to2,
+      force: true
+    }], {}),
     new ExtraneousFileCleanupPlugin(opts),
     new webpack.DefinePlugin({
       'process.env.ringCentralConfigs': JSON.stringify(sysConfigDefault.ringCentralConfigs),
